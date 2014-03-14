c4b04dfab919be86aa0a8faf7b91f3f1db99475e
Jason <jason.kastrup@gmail.com>
2014-03-13 22:02:03 -0400

    Expressions Assignment
    Expressions Industry idea thought up.

--- Expressions/Expressions_Industry/js/script.js
+++ Expressions/Expressions_Industry/js/script.js
@@ -1,3 +1,11 @@
 /**
  * @author Jason Kastrup
+ * Expressions Assignment: Industry
+ * 3/13/2014
+ * 
+ * Emails are extremely important in any industry today.  Many users often send poorly written emails, full of grammatical errors inappropriate content.
+ * Although I cannot teach English, I can create an e-mail contact signature for anyone to use at the bottom of their emails.
  */
+
+var numAccnts = parseInt(prompt("How many contact accounts do you have? I.e. E-mail, Facebook, Twitter, etc."));
+
