489a91061654c93e1d60db99c7f2772cc4db34d7
Jason <jason.kastrup@gmail.com>
2014-03-13 22:57:08 -0400

    Worksheet Problem 4
    Updated variable information and calculations

--- Expressions_worksheet/Expressions_Worksheet/js/script.js
+++ Expressions_worksheet/Expressions_Worksheet/js/script.js
@@ -37,7 +37,43 @@ console.log("Each person ate " + numEat + " slices of pizza at the party.");
  * Assuming the guests only eat WHOLE slice, how many slices will Sparky get?
  * 
  */
+console.log("PROBLEM 3");
 
 var numSparky = (numPizzas * numSlices) - (Math.floor(numEat) * numParty);
 
 console.log("Sparky got " + numSparky + " slices of pizza!");
+
+/**
+ * Problem 4: Average shopping bill
+ * Create an expression that will use the items in the array to calculate the average amount spent on groceries.
+ */
+console.log("Problem 4");
+
+var weeklyTotals = [123,54,75,45,110];
+var numWeeks = weeklyTotals.length;
+
+var shopTotal; // Total for all 5 weeks of shopping
+
+for(i=0;i<numWeeks;i++){
+	shopTotal += weeklyTotals[i];
+}
+
+var avgCost = shopTotal / numWeeks;
+
+console.log("You have spent a total of $" + shopTotal + " on groceries over 5 weeks.  That is an average of $" + avgCost + " per week");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
