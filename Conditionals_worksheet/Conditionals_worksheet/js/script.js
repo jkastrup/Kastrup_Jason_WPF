32df4dc16fa6047fc1bc39afe63d5c93488e9521
Jason <jason.kastrup@gmail.com>
2014-03-19 00:28:59 -0400

    Con_WS 3
    Problem 1 completed

--- Conditionals_worksheet/Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/Conditionals_worksheet/js/script.js
@@ -11,8 +11,10 @@
 
 var compWeight = 275;  // Weight of the competitor
 
-if(compWeight >= 250){
-	
+if(compWeight >= 250){ 
+	// If the competitor's weight is greater than or equal to 250
+	console.log("The competitor qualifies for the heavyweight division.");
 }else{
-	
+	// Otherwise, competitor needs to gain weight
+	console.log("The competitor needs to gain some weight!");
 }
