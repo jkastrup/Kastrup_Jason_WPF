c6ed9d878f166994e1c427dee173499263673fb7
Jason <jason.kastrup@gmail.com>
2014-03-19 00:26:51 -0400

    Conditionals Worksheet 2
    Started problem 1

--- Conditionals_worksheet/Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/Conditionals_worksheet/js/script.js
@@ -4,4 +4,15 @@
  * Conditionals Worksheet
  */
 
+/**
+ * Problem 1: Stuff Your Face
+ * Is the contestant 250 lbs or more?
+ */
 
+var compWeight = 275;  // Weight of the competitor
+
+if(compWeight >= 250){
+	
+}else{
+	
+}
