d7e510e54c64187397e852b67d50032655862e85
Jason <jason.kastrup@gmail.com>
2014-03-20 21:07:35 -0400

    Conditions Assignment: Personal
    Variables initialized

--- Conditions_Personal/Conditions_Personal/js/script.js
+++ Conditions_Personal/Conditions_Personal/js/script.js
@@ -1,3 +1,13 @@
 /**
  * @author Jason Kastrup
+ * Conditions Assignment:  Personal
+ * 3/20/2014
+ * 
+ * What should I eat right now?
  */
+
+var isHungry = false;
+var timeOfDay = "";
+var inOrOut = "";
+
+
