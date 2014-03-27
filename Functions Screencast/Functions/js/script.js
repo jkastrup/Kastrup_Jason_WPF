eedca9c74a92cdbd85939e01dd7463ac5f07cdd7
Jason <jason.kastrup@gmail.com>
2014-03-26 21:33:56 -0400

    Functions - Basic Structure

---
+++ Functions Screencast/Functions/js/script.js
@@ -0,0 +1,17 @@
+/**
+ * @author Jason Kastrup
+ * Screencast Functions
+ * Functions - Basic Structure
+ */
+
+function outpMsg(){
+	console.log("Hello World");
+}
+
+function calcArea(){
+	var width = 20;
+	var height = 30;
+	vaf area = width * height;
+	console.log(aera);
+}
+
