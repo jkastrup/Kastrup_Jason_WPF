0190acae0794f86256bb39d30213cb0caa609cc8
Jason <jason.kastrup@gmail.com>
2014-03-27 20:19:39 -0400

    Functions_Personal

---
+++ Functions_Industry/Functions_Industry/js/script.js
@@ -0,0 +1,9 @@
+/**
+ * @author Jason Kastrup
+ * Functions Assignment: Industry
+ * 
+ * 
+ */
+
+
+
--- Functions_Personal/Functions_Personal/js/script.js
+++ Functions_Personal/Functions_Personal/js/script.js
@@ -3,8 +3,17 @@
  * Functions Assignments: Personal
  * 3/27/14
  * 
- * Below is code to 
+ * Below is code to calculat a player's KDA (kills + deaths)/Assists in any game that has those statistics
  */
 
+var yourKDA;	// Keeps track of the user's KDA
+var numKills;	// The number of kills the user has
+var numDeaths;	// The number of deaths the user has
+var numAssists;	// The number of assists the user has
 
+
+function calcKDA(kills, deaths, assists){
+	var kda = (kills + assists) / deaths;
+	return kda;
+}
 
