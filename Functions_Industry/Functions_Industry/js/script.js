ec11d040d35a2960c12f8b6a6b0e9a54c67acffa
Jason <jason.kastrup@gmail.com>
2014-03-31 12:37:50 -0400

    Functions_Assignment Recommit

--- Functions_Industry/Functions_Industry/js/script.js
+++ Functions_Industry/Functions_Industry/js/script.js
@@ -1,9 +1,18 @@
 /**
  * @author Jason Kastrup
+ * 3/27/14
  * Functions Assignment: Industry
  * 
- * 
+ * Having issues coming up with program ideas
+ * Below is a basic program showing the use of anonymous functions
  */
 
 
+var input = parseInt(prompt("Please enter a '1' or a '2'"));	// Gets input from the user
 
+(input === 1) ? 
+
+
+var myFunction = function(){
+	console.log("Your input was " + input);
+}
